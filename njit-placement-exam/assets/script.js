d11f9d3b48fc7d7d87e469a6f1bfce03a55aba2e
bgentoso <88010969+bgentoso@users.noreply.github.com>
2021-08-23 19:08:03 -0400

    Add files via upload

---
+++ njit-placement-exam/assets/script.js
@@ -0,0 +1,52 @@
+// HINT: You can delete this console.log after you no longer need it!
+console.log('JavaScript code has loaded!')
+
+// First, tell us your name
+let yourName = "Brian Gentoso" // HINT: Replace this with your own name!
+
+// We'll use these variables to track the counts of each cookie type
+let gb = 0      // Ginger bread
+let cc = 0      // Chocolate Chip
+let sugar = 0   // Sugar Sprinkle
+let total = 0
+
+// Code to update name display 
+document.getElementById('credit').textContent = `Created by ${yourName}`
+
+// Event listener for clicks on the "+" button for Ginger Bread cookies
+document.getElementById('add-gb').addEventListener('click', function() {
+    gb += 1;
+    total +=1;
+    document.getElementById('qty-gb').textContent = gb
+    document.getElementById('qty-total').textContent = total
+})
+document.getElementById('minus-gb').addEventListener('click', function() {
+    gb -= 1;
+    total -=1;
+    document.getElementById('qty-gb').textContent = gb
+    document.getElementById('qty-total').textContent = total
+})
+document.getElementById('add-cc').addEventListener('click', function() {
+    cc += 1;
+    total +=1;
+    document.getElementById('qty-cc').textContent = cc
+    document.getElementById('qty-total').textContent = total
+})
+document.getElementById('minus-cc').addEventListener('click', function() {
+    cc -= 1;
+    total -=1;
+    document.getElementById('qty-cc').textContent = cc
+    document.getElementById('qty-total').textContent = total
+})
+document.getElementById('add-sugar').addEventListener('click', function() {
+    sugar += 1;
+    total +=1;
+    document.getElementById('qty-sugar').textContent = sugar
+    document.getElementById('qty-total').textContent = total
+})
+document.getElementById('minus-sugar').addEventListener('click', function() {
+    sugar -= 1;
+    total -=1;
+    document.getElementById('qty-sugar').textContent = sugar
+    document.getElementById('qty-total').textContent = total
+})
